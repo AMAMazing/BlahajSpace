3e96c10c9c4c714e870bb180df1152df232ea348
AMA Mazing <alexmalone489@gmail.com>
2022-09-24 14:43:11 +1000

    Cube

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import styled from "styled-components"
+
+import { Canvas } from "@react-three/fiber" 
+import { OrbitControls } from "@react-three/drei"
+import Box from "../components/Box"
+
+export default function Home() {
+  return (
+    <Wrapper className="App">
+      <h1 style={{textAlign: "center", fontSize: "50px"}}>Test</h1>
+      <Canvas className="canvas"> 
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Box />
+      </Canvas>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+
+  canvas {
+    height: 500px;
+  }
+`;
